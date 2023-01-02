3d883313f38f32f72a08742064d48b633b038ae8
Ahmed Albahrani <alias.co>
2023-01-02 12:52:30 +0300

    Update Routes file for devise models

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json },  controllers: {
-        sessions: 'users/sessions',
-        registrations: 'users/registrations'  
+  devise_for :admins, defaults: { format: :json },  controllers: {
+        sessions: 'admins/sessions',
+        registrations: 'admins/registrations'  
       }
-  resources :users
+  devise_for :shop_owners, defaults: { format: :json },  controllers: {
+        sessions: 'shop_owners/sessions',
+        registrations: 'shop_owners/registrations'  
+      }
+  devise_for :buyers, defaults: { format: :json },  controllers: {
+        sessions: 'buyers/sessions',
+        registrations: 'buyers/registrations'  
+      }
+  devise_for :cashiers, defaults: { format: :json },  controllers: {
+        sessions: 'cashiers/sessions',
+        registrations: 'cashiers/registrations'  
+      }  
+  resources :admins
+  resources :shop_owners
+  resources :buyers
+  resources :cashiers
   resources :invoices
   resources :orders
   resources :shops
